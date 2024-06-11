7033ec66edac602ed5ba3fa286407339d457f6ee
Sai Bulusu <sai.bulusu@gmail.com>
2024-06-10 20:30:23 -0700

    Update update-readme.sh

--- update-readme.sh
+++ update-readme.sh
@@ -4,7 +4,9 @@ DIRECTORY="./"  # You can change this to the specific directory path if needed
 # Iterate through all .cpp files in the directory
 for file in "$DIRECTORY"/*.cpp; do
   if [[ -f "$file" ]]; then  # Check if the file exists
-    echo "Processing $file..."
+    file_name=$(basename "$file")
+    file_name="${file_name%.cpp}"
+    echo "Processing $file_name..."
   else
     echo "No .cpp files found in $DIRECTORY."
   fi
