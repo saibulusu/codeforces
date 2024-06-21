fe7501f120d058107074085e11b07c1eba5a0ef5
saibulusu <sai.bulusu@gmail.com>
2024-06-20 23:20:17 -0700

    Working on 2B.

---
+++ src/2B.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<vector<pair<int, int>>> pairs;
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      int number;
+      cin >> number;
+
+      int pow2 = 0, pow5 = 0;
+      while (number > 0) {
+      }
+    }
+  }
+
+  return 0;
+}
